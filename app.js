42c57db29eb1e0e7465a15d4b53d31961affac5d
Aaron Uhr <jaewon.uhr@gmail.com>
2020-08-03 02:53:25 -0400

    added chat functionality

--- app.js
+++ app.js
@@ -1,44 +1,37 @@
-var bodyParser     = require("body-parser"),
-    mongoose       = require("mongoose"),
-    // passport       = require("passport"),
-    cookieParser   = require("cookie-parser"),
-    // LocalStrategy  = require("passport-local"),
-    flash          = require("connect-flash"),
-    session        = require("express-session"),
-    methodOverride = require("method-override");
-    methodOverride = require("method-override");
+var bodyParser     = require("body-parser");
 
 var express = require("express");
+var path = require('path');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-app.use(session({
-    secret: 'dog',
-    resave: false,
-    saveUninitialized: false
-}));
 
 server.listen(port, function(){
     console.log("listening on port 3000");
 });
 
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+var numUsers = 0;
+
 app.use(bodyParser.urlencoded({extended: true}));
-// app.engine('html', require('ejs').renderFile);
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
 
 //--------------------------------------------------
 
 io.on('connection', function(socket){
+
+    //VIDEO========================================
+    
     io.to(socket.handshake.query.roomName).emit('newUser', socket.handshake.query.userName);
     socket.join(socket.handshake.query.roomName, function(){
         io.to('${socketId}').emit('newUser');
     });
-    // socket.on('end', function(){
-    //     console.log('State: end');
-    // });
     socket.on('requestCurrentVideo', function(){
         //emit a signal that asks for current videoID and time
     });
@@ -58,17 +51,69 @@ io.on('connection', function(socket){
         console.log('Playback change: '+rate);
         io.to(room).emit('playbackPlayer', rate);
     });
-    // socket.on('status5', function(time){
-    //     console.log('5');
-    // });
-    // socket.on('statusminus1', function(time){
-    //     console.log('-1');
-    //     // io.emit('pausePlayer');
-    // });
     socket.on('newVideo', function(url, room){
         console.log('New Video!');
+        console.log(url);
         var newVideoID = url.split('v=')[1];
         io.to(room).emit('newVideo', newVideoID);
+    });
+
+    //CHAT===========================================
+
+    var addedUser = false;
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', (data) => {
+      // we tell the client to execute 'new message'
+      socket.broadcast.emit('new message', {
+        username: socket.username,
+        message: data
+      });
+    });
+  
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', (username) => {
+      if (addedUser) return;
+  
+      // we store the username in the socket session for this client
+      socket.username = username;
+      ++numUsers;
+      addedUser = true;
+      socket.emit('login', {
+        numUsers: numUsers
+      });
+      // echo globally (all clients) that a person has connected
+      socket.broadcast.emit('user joined', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    });
+  
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+      socket.broadcast.emit('typing', {
+        username: socket.username
+      });
+    });
+  
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+      socket.broadcast.emit('stop typing', {
+        username: socket.username
+      });
+    });
+  
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+      if (addedUser) {
+        --numUsers;
+  
+        // echo globally that this client has left
+        socket.broadcast.emit('user left', {
+          username: socket.username,
+          numUsers: numUsers
+        });
+      }
     });
 });
 
@@ -92,93 +137,3 @@ app.get("/room/:id", function(req, res){
 app.get('/', function (req, res) {
     res.send("404 Error");
 });
-
-
-
-
-
-
-
-
-
-
-// mongoose.Promise = global.Promise;
-// const databaseUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/youtube-sync';
-
-// mongoose.connect(databaseUri, { useNewUrlParser: true })
-//       .then(() => console.log(`Database connected`))
-//       .catch(err => console.log(`Database connection error: ${err.message}`));
-
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.set("view engine", "ejs");
-// app.use(express.static(__dirname + "/public"));
-// app.use(methodOverride('_method'));
-// app.locals.moment = require('moment');
-
-// var server = app.listen(3000, function(){
-//     console.log("Server listening on port 3000");
-// });
-
-// var io = socket(server);
-
-// io.on("connection", function(socket){
-//     console.log("Made socket connection");
-// });
-
-// app.get("/", function(req, res){
-//     res.render("index");
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // ----------Functions-------------------------------------
-
-// function getVideos() {
-//     var query = document.getElementById('url').value;
-//     $.ajax({
-//       url: '/youtube_query',
-//       type: 'POST',
-//       cache: false, 
-//       data: {query: query}, 
-//       success: function(data){
-//         document.getElementById('videoResults').innerHTML=data;
-//       }, 
-//       error: function(jqXHR, textStatus, err){}
-//    })
-// }
-// // 2. This code loads the IFrame Player API code asynchronously.
-// var tag = document.createElement('script');
-// tag.src = "https://www.youtube.com/player_api";
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-// // 3. This function creates an <iframe> (and YouTube player)
-// //    after the API code downloads.
-// var player;
-// function onYouTubePlayerAPIReady() {
-// player = new YT.Player('ytplayer', {
-//   width:800,
-//   height:500,
-//   events: {
-//     'onStateChange': onPlayerStateChange,
-//     'onReady': onPlayerReady,
-//     'onPlaybackRateChange': onPlaybackRateChange
-//   }
-// });
-// }
-// // 4. The API will call this function when the video player is ready.
-// function onPlayerReady(event) {
-//     // do stuff here if necessary
-// }
